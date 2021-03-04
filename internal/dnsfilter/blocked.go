7cd835057725a825410009910ac84e9ec3a7aec2
Doquanganh <dqanh@mic.gov.vn>
2021-03-04 14:48:11 +0700

    add domain block ZALO

--- internal/dnsfilter/blocked.go
+++ internal/dnsfilter/blocked.go
@@ -19,7 +19,7 @@ type svc struct {
 // client/src/helpers/constants.js
 // client/src/components/ui/Icons.js
 var serviceRulesArray = []svc{
-	{"zalo", []string{"||zalo.me/pc^", "||zalo.me^"}},
+	{"zalo", []string{"||zalo.me/pc^", "||zalo.me^","||zaloapp.com^"}},
 	{"whatsapp", []string{"||whatsapp.net^", "||whatsapp.com^"}},
 	{"facebook", []string{
 		"||facebook.com^",
